7e2f0dbb70dde410bcead0be352b354db460ffc9
Bharti277 <bhartideependra277@gmail.com>
2024-11-12 16:14:53 +0530

    check if an array is sorted

---
+++ Array/array_is_sorted.js
@@ -0,0 +1,13 @@
+let arr = [95, 10, 5, 56, 78, 85, 34];
+// let arr = [10, 50, 56, 78, 85, 340];
+
+function isArraySorted() {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+isArraySorted();
