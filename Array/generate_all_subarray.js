4ecda61eeb6cc5ed1eb9788aa2a99388f822771d
Bharti277 <bhartideependra277@gmail.com>
2024-11-14 17:07:33 +0530

    generate all subarray

---
+++ Array/generate_all_subarray.js
@@ -0,0 +1,18 @@
+let arr = [0, 1, 2, 3, 4];
+let n = arr.length;
+
+function generateAllSubarray() {
+  let sub_arrays = [];
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i; j++) {
+      let sub_array = [];
+      for (let k = i; k <= j + i; k++) {
+        sub_array.push(arr[k]);
+      }
+      sub_arrays.push(sub_array);
+    }
+  }
+  return sub_arrays;
+}
+
+generateAllSubarray();
