64db18c19778342dc59fff0173d17dfe21f82e04
Bharti277 <bhartideependra277@gmail.com>
2024-12-03 15:46:17 +0530

    duplicates

--- Strings/print_duplicates.js
+++ Strings/print_duplicates.js
@@ -1,18 +1,36 @@
 // Print all the duplicate characters in a string
 
+// First Method
 let str = "geeksforgeekss";
 
 function printDuplicate() {
-    let charCount = {};
-    let duplicates = [];
-    for(let ch of str){
-        charCount[ch] = (charCount[ch] || 0) + 1;
+  let charCount = {};
+  let duplicates = [];
+  for (let ch of str) {
+    charCount[ch] = (charCount[ch] || 0) + 1;
+  }
+  for (let ch in charCount) {
+    if (charCount[ch] > 1) {
+      duplicates.push(ch);
     }
-    for(let ch in charCount){
-        if(charCount[ch]>1){
-            duplicates.push(ch)
-        }
-    }
-    return duplicates;
+  }
+  return duplicates;
 }
 printDuplicate();
+
+// second method
+
+function printAllDuplicate() {
+  str = str.split("").sort().join("");
+  for (let i = 0; i < str.length; i++) {
+    let count = 1;
+    while (i < str.length - 1 && str[i] === str[i + 1]) {
+      i++;
+      count++;
+    }
+    if (count > 1) {
+      console.log(str[i] + "count" + count);
+    }
+  }
+}
+printAllDuplicate();
