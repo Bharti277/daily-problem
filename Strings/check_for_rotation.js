75ace4e786d25baa805a5010adab5ae3dfd09f73
Bharti277 <bhartideependra277@gmail.com>
2024-12-19 13:47:03 +0530

    rotation

---
+++ Strings/check_for_rotation.js
@@ -0,0 +1,10 @@
+function checkRotation(s1, s2) {
+  let n = s1.length;
+  for (let i = 0; i < n; i++) {
+    if (s1 === s2) return true;
+    let last = s1[s1.length - 1];
+    s1 = last + s1.slice(0, s1.length - 1);
+  }
+  return false;
+}
+checkRotation("abcd", "cdab");
