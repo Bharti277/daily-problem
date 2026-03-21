029f5436e2c70a98d56d708f5e47478d573cd597
Bharti277 <bhartideependra277@gmail.com>
2026-03-21 10:02:37 +0530

    sort array in descending order

---
+++ Array/sort_array.js
@@ -0,0 +1,16 @@
+let arr = [95, 10, 5, 56, 95, 78, 85, 34];
+
+function sortArray() {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(sortArray());
